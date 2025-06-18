d090655ae9b638c11f6d7e6c91407613cf487589
senfor <senfor@guojiang.tv>
2025-06-18 18:14:54 +0800

    versiont 1.0.0.0

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,7 @@
 import Flutter
+import AppTrackingTransparency
 import UIKit
+
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +9,12 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      DispatchQueue.main.asyncAfter(deadline: .now() + 3.6) {
+          if #available(iOS 14, *) {
+              ATTrackingManager.requestTrackingAuthorization { status in
+              }
+          }
+      }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
